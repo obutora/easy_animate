a810230c8dd9263c8b2766ea799d99351f27fde7
chan haga <obutora@gmail.com>
2023-03-21 17:57:42 +0900

    feat :fade in animation.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:simple_animations/simple_animations.dart';
 import 'package:easy_animate/mock/mock_box.dart';
 import 'package:easy_animate/animation/pulse_animation.dart';
 import 'package:easy_animate/animation/shake_animation.dart';
+import 'package:easy_animate/animation/fade_in_animation.dart';
 
 void main() {
   runApp(const HomeScreen());
@@ -21,8 +22,13 @@ class _HomeScreenState extends State<HomeScreen> {
   final List<String> list = <String>[
     'BouncingAnimation',
     'PulseAnimation',
-    'ShakeAnimation',
-    'Four'
+    'ShakeAnimation( isHorizontal : true)',
+    'ShakeAnimation( isHorizontal : false)',
+    'FadeInAnimation( FadeInAxis : none)',
+    'FadeInAnimation( FadeInAxis : top)',
+    'FadeInAnimation( FadeInAxis : right)',
+    'FadeInAnimation( FadeInAxis : bottom)',
+    'FadeInAnimation( FadeInAxis : left)',
   ];
 
   String selectedValue = 'BouncingAnimation';
@@ -74,13 +80,51 @@ Widget animationSelector(String selectedValue) {
         developerMode: true,
         child: MockBox(),
       );
-    case 'ShakeAnimation':
+    case 'ShakeAnimation( isHorizontal : true)':
       return const ShakeAnimation(
-        developerMode: false,
+        developerMode: true,
+        isHorizontal: true,
+        child: MockBox(),
+      );
+    case 'ShakeAnimation( isHorizontal : false)':
+      return const ShakeAnimation(
+        developerMode: true,
+        isHorizontal: false,
+        child: MockBox(),
+      );
+    case 'FadeInAnimation( FadeInAxis : none)':
+      return const FadeInAnimation(
+        developerMode: true,
+        fadeInAxis: FadeInAxis.none,
+        child: MockBox(),
+      );
+    case 'FadeInAnimation( FadeInAxis : top)':
+      return const FadeInAnimation(
+        developerMode: true,
+        fadeInAxis: FadeInAxis.top,
+        child: MockBox(),
+      );
+    case 'FadeInAnimation( FadeInAxis : right)':
+      return const FadeInAnimation(
+        developerMode: true,
+        fadeInAxis: FadeInAxis.right,
+        child: MockBox(),
+      );
+    case 'FadeInAnimation( FadeInAxis : bottom)':
+      return const FadeInAnimation(
+        developerMode: true,
+        fadeInAxis: FadeInAxis.bottom,
+        child: MockBox(),
+      );
+    case 'FadeInAnimation( FadeInAxis : left)':
+      return const FadeInAnimation(
+        developerMode: true,
+        fadeInAxis: FadeInAxis.left,
         child: MockBox(),
       );
     default:
       return const BouncingAnimation(
+        developerMode: true,
         child: MockBox(),
       );
   }
---
+++ lib/animation/fade_in_animation.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:simple_animations/animation_builder/play_animation_builder.dart';
+import 'package:simple_animations/movie_tween/movie_tween.dart';
+
+class FadeInAnimation extends StatelessWidget {
+  const FadeInAnimation({
+    super.key,
+    required this.child,
+    this.fadeInAxis = FadeInAxis.none,
+    this.durationMilliseconds = 1200,
+    this.moveAmount = 100,
+    this.developerMode = false,
+  });
+
+  final Widget child;
+
+  /// [fadeInAxis] is the axis of the fade in animation.
+  final FadeInAxis fadeInAxis;
+
+  final double durationMilliseconds;
+
+  /// [moveAmount] is the amount of pixels the widget.
+  final double moveAmount;
+
+  final bool developerMode;
+
+  calcMovement(double value) {
+    switch (fadeInAxis) {
+      case FadeInAxis.none:
+        return const Offset(0, 0);
+      case FadeInAxis.top:
+        return Offset(0, value);
+      case FadeInAxis.right:
+        return Offset(-value, 0);
+      case FadeInAxis.bottom:
+        return Offset(0, -value);
+      case FadeInAxis.left:
+        return Offset(value, 0);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final tween = MovieTween()
+      ..tween(
+        'opacity',
+        Tween<double>(begin: 0, end: 1),
+        duration: Duration(milliseconds: durationMilliseconds.toInt()),
+        curve: Curves.ease,
+      )
+      ..tween(
+        'move',
+        Tween<double>(begin: -moveAmount, end: 0),
+        duration: Duration(milliseconds: durationMilliseconds.toInt()),
+        curve: Curves.easeInOutCirc,
+      );
+
+    return PlayAnimationBuilder(
+        tween: tween,
+        duration: tween.duration,
+        developerMode: developerMode,
+        builder: ((context, value, child) {
+          return Transform.translate(
+              offset: calcMovement(value.get('move')),
+              child: Opacity(opacity: value.get('opacity'), child: child));
+        }),
+        child: child);
+  }
+}
+
+enum FadeInAxis {
+  none,
+  top,
+  right,
+  bottom,
+  left,
+}
